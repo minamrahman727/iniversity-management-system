a194fb19cc4bd7647b743e97f8a4d48f0d9911d7
syed minam ur rehman <138588241+minamrahman727@users.noreply.github.com>
2023-11-04 12:01:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,195 @@
+function showStudents() {
+    const content = document.getElementById('content');
+    content.innerHTML = `
+        <h2>Students</h2>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>phone no</th>
+            </tr>
+            <tr>
+                <td>001</td>
+                <td>syed minam ur rehman</td>
+                <td>minamrahman727@gmail.com</td>
+                <td>03369289269</td>
+            </tr>
+            <tr>
+                <td>002</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>003</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>004</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>005</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>006</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>007</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>008</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>009</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>010</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>011</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>012</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>013</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>014</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>015</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>016</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>017</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>018</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>019</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>020</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+            <tr>
+                <td>021</td>
+                <td>john doe</td>
+                <td>samplemail@web.com</td>
+                <td>+1 555 555 5555</td>
+            </tr>
+
+            <!-- Add student data here -->
+        </table>
+    `;
+}
+
+function showCourses() {
+    const content = document.getElementById('content');
+    content.innerHTML = `
+        <h2>Courses</h2>
+        <table>
+            <tr>
+                <th>course id</th>
+                <th>Name</th>
+                <th>duration</th>
+            </tr>
+            <tr>
+                <td>01</td>
+                <td>web3.0,metaverse,generative ai and solopreneur</td>
+                <td>1 year</td>
+            </tr>
+            <tr>
+                <td>02</td>
+                <td>typescript fundamental</td>
+                <td>3 months</td>
+            </tr>
+            <tr>
+                <td>03</td>
+                <td>basic web development</td>
+                <td>6 months</td>
+            </tr>
+            <tr>
+                <td>04</td>
+                <td>phython basics</td>
+                <td>5 months</td>
+            </tr>
+            <!-- Add course data here -->
+        </table>
+    `;
+}
+
+function showGrades() {
+    const content = document.getElementById('content');
+    content.innerHTML = `
+        <h2>Grades</h2>
+        <table>
+            <tr>
+                <th>Student ID</th>
+                <th>Course ID</th>
+                <th>Grade</th>
+            </tr>
+        <h1>something went wrong!</h1>
+        <h4>or</h4>
+        <h3>Grades are available after tests</h3>
+            <!-- Add grade data here -->
+        </table>
+    `;
+}
